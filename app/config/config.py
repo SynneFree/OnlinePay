92b35c6d79deecf874a75c44d655037fafb75581
SynneFree <veronicatjan@hotmail.com>
2019-06-29 18:28:39 +0800

    update final version

--- app/config/config.py
+++
@@ -1,5 +0,0 @@
-class Config(object):
-    MYSQL_DATABASE_USER = 'root'
-    MYSQL_DATABASE_PASSWORD = 'lyx09241021/?'
-    MYSQL_DATABASE_DB = 'onlinepayment'
-    MYSQL_DATABASE_HOST = 'localhost'
